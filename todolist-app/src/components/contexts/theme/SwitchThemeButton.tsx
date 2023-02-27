552d0abcb30ad6aa3209b17d338cdac632d47ae9
Jiří Kneifl <Jirka.Kneifl@email.cz>
2023-02-27 14:37:32 +0100

    pridani zemn pri kliknuti na themeSwitcher do localstorage -> ted uz se meni cela aplikace

--- todolist-app/src/components/contexts/theme/SwitchThemeButton.tsx
+++ todolist-app/src/components/contexts/theme/SwitchThemeButton.tsx
@@ -1,10 +1,21 @@
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
 import {ThemeContext, themeOptions} from "./Theme";
 
 
 function SwitchThemeButton(){
     const { theme, setTheme } = useContext(ThemeContext);
 
+    useEffect(() =>{
+        const savedTheme = localStorage.getItem("theme");
+        if(savedTheme && Object.keys(themeOptions).includes(savedTheme)){
+            setTheme(savedTheme);
+        }
+    }, []);
+
+    const handleThemeChange = (newTheme: string) => {
+        setTheme(newTheme);
+        localStorage.setItem("theme", newTheme);
+    }
 
     return(
         <>
@@ -12,7 +23,7 @@ function SwitchThemeButton(){
                 <button
                     key={key}
                     style={{ backgroundColor: color }}
-                    onClick={() => setTheme(key)}
+                    onClick={() => handleThemeChange(key)}
                     disabled={theme === key}
                 >
                     {key}
