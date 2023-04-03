272186da4bf400e7c846a529f835c88d19681655
Jiří Kneifl <Jirka.Kneifl@email.cz>
2023-04-03 19:52:23 +0200

    dodelani upraveni a mazani todocek

--- todolist-app/src/components/structureComponents/ToDo/ToDo.tsx
+++ todolist-app/src/components/structureComponents/ToDo/ToDo.tsx
@@ -1,52 +1,71 @@
-import Button from "../../basicComponents/Button/Button";
-import HStack from "../../basicComponents/HStack";
-import "./ToDo.css";
-import {Link, useNavigate, useParams} from "react-router-dom";
+import HStack from '../../basicComponents/HStack';
+import './ToDo.css';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { FiTrash, FiEdit } from 'react-icons/fi';
-import VStack from "../../basicComponents/VStack";
-import Text from "../../basicComponents/Text/Text";
+import Text from '../../basicComponents/Text/Text';
+import { getDecodedToken } from '../../../utils/getDecodedToken';
 
-type toDoProps ={
-    toDoLabel: string
+type toDoProps = {
+    toDoLabel: string;
     idTodo: any;
-}
+    userId: number;
+};
 
 function ToDo(props: toDoProps) {
+    const user = getDecodedToken(localStorage.getItem('access_token') as string);
+    const userId = user.id;
     const { idList } = useParams();
     const navigate = useNavigate();
 
     return (
         <>
-        <div className={"todoBody"}>
-            <HStack gap={8} justifyContent={"space-between"} alignItems={"center"}>
-                <HStack gap={4} justifyContent={"center"} alignItems={"center"}>
-                    <input type="checkbox"/>
-                    <Text type={"body"}>{props.toDoLabel}</Text>
+            <div className={'todoBody'}>
+                <HStack gap={8} justifyContent={'space-between'} alignItems={'center'}>
+                    <HStack gap={4} justifyContent={'center'} alignItems={'center'}>
+                        <input type="checkbox" />
+                        <Text type={'body'}>{props.toDoLabel}</Text>
+                    </HStack>
+                    <HStack gap={4}>
+                        {/*<Button label={"Archvate"}></Button> -> archvovabni na posledy*/}
+                        <button className={'onlyIconButton'}>
+                            <Link to={`/main/${userId}/${idList}/updateTodo/${props.idTodo}`}>
+                                <FiEdit size={28} className={'editTodoIcon'} />
+                            </Link>
+                        </button>
+                        <button
+                            className={'onlyIconButton'}
+                            onClick={(e) => {
+                                fetch(
+                                    `http://localhost:3001/main/${userId}/${idList}/deleteTodo/${props.idTodo}`,
+                                    {
+                                        method: 'DELETE',
+                                        headers: {
+                                            Authorization: `Bearer ${localStorage.getItem(
+                                                'access_token',
+                                            )}`,
+                                        },
+                                    },
+                                )
+                                    .then((response) => {
+                                        if (!response.ok) {
+                                            throw new Error('Something went wrong');
+                                        }
+                                        navigate(0);
+                                    })
+                                    .catch((e) => {
+                                        console.log(e);
+                                    });
+                            }}
+                        >
+                            <Link to={''}>
+                                <FiTrash size={28} className={'deleteTodoIcon'} />
+                            </Link>
+                        </button>
+                    </HStack>
                 </HStack>
-                <HStack gap={4}>
-                    {/*<Button label={"Archvate"}></Button> -> archvovabni na posledy*/}
-                    <button className={"onlyIconButton"}><Link to={`/main/${idList}/updateTodo/${props.idTodo}`}><FiEdit size={28} className={"editTodoIcon"}/></Link></button>
-                    <button className={"onlyIconButton"} onClick={(e) => {
-                        fetch(`http://localhost:3001/main/${idList}/deleteTodo/${props.idTodo}`, { method: "DELETE" })
-                            .then((response) => {
-                                if(!response.ok){
-                                    throw new Error('Something went wrong');
-                                }
-                                navigate(0);
-                            })
-                            .catch((e) => {
-                                console.log(e);
-                            });
-                    }}>
-                        <Link to={''}>
-                            <FiTrash size={28} className={"deleteTodoIcon"}/>
-                        </Link>
-                    </button>
-                </HStack>
-            </HStack>
-        </div>
+            </div>
         </>
-    )
+    );
 }
 
 export default ToDo;
--- todolist-app/src/components/structureComponents/UpdateTodoForm/UpdateTodoForm.tsx
+++ todolist-app/src/components/structureComponents/UpdateTodoForm/UpdateTodoForm.tsx
@@ -1,26 +1,28 @@
-import Text from "../../basicComponents/Text/Text";
-import VStack from "../../basicComponents/VStack";
-import * as React from "react";
-import {FormEvent, useState} from "react";
-import {useParams} from "react-router-dom";
-import {useMutation, useQuery} from "react-query";
+import Text from '../../basicComponents/Text/Text';
+import VStack from '../../basicComponents/VStack';
+import * as React from 'react';
+import { FormEvent, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useMutation, useQuery } from 'react-query';
 import './UpdateTodoForm.css';
 import { useNavigate } from 'react-router-dom';
-import {FormattedMessage} from "react-intl";
+import { FormattedMessage } from 'react-intl';
+import { getDecodedToken } from '../../../utils/getDecodedToken';
 
-
-function UpdateTodoForm(){
+function UpdateTodoForm() {
+    const user = getDecodedToken(localStorage.getItem('access_token') as string);
     const navigate = useNavigate();
     const { idList, idTodo } = useParams();
     const [title, setTitle] = useState('');
 
-    const {
-        isLoading,
-        isError,
-        error
-    } = useQuery<unknown, Error>(
-        ['todo', idList, idTodo ],
-        () => fetch(`http://localhost:3001/main/${idList}/updateTodo/${idTodo}`).then((res) => res.json()),
+    const { isLoading, isError, error } = useQuery<unknown, Error>(
+        ['todo', idList, idTodo],
+        () =>
+            fetch(`http://localhost:3001/main/${user.id}/${idList}/updateTodo/${idTodo}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+                },
+            }).then((res) => res.json()),
         {
             onSuccess: (data: any) => {
                 setTitle(data.title);
@@ -29,20 +31,25 @@ function UpdateTodoForm(){
     );
 
     const updateTodoMutation = useMutation('updateTodo', () =>
-        fetch(`http://localhost:3001/main/${idList}/updateTodo/${idTodo}`, {
+        fetch(`http://localhost:3001/main/${user.id}/${idList}/updateTodo/${idTodo}`, {
             method: 'PUT',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ title })
-        }).then(()=>{
-            console.log("updateTodoMutation");
-            navigate(`/main/${idList}`)
-        }).catch()
-    )
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+            },
+            body: JSON.stringify({ title }),
+        })
+            .then(() => {
+                console.log('updateTodoMutation');
+                navigate(`/main/${user.id}/${idList}`);
+            })
+            .catch(),
+    );
 
     const handleUpdateSubmit = (e: FormEvent) => {
         updateTodoMutation.mutate();
         e.preventDefault();
-    }
+    };
 
     if (isLoading) return <p>Loading</p>;
     if (isError) return <p>Error: {error?.message}</p>;
@@ -50,22 +57,29 @@ function UpdateTodoForm(){
     return (
         <>
             <form onSubmit={handleUpdateSubmit}>
-                <Text type={"title"} children={<FormattedMessage id={"title.updateTodoForm.updateTodo"}/> }/>
+                <Text
+                    type={'title'}
+                    children={<FormattedMessage id={'title.updateTodoForm.updateTodo'} />}
+                />
                 <VStack gap={1}>
-                    <Text type={"body"} children={<FormattedMessage id={"label.title"}/>}></Text>
+                    <Text type={'body'} children={<FormattedMessage id={'label.title'} />}></Text>
                     <input
-                        className={"titleInput"}
-                        type={"text"}
+                        className={'titleInput'}
+                        type={'text'}
                         name={title}
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}
                     />
 
-                    <button className={"updateButton"} type={"submit"} children={<FormattedMessage id={"label.update"}/>}></button>
+                    <button
+                        className={'updateButton'}
+                        type={'submit'}
+                        children={<FormattedMessage id={'label.update'} />}
+                    ></button>
                 </VStack>
             </form>
         </>
-    )
+    );
 }
 
 export default UpdateTodoForm;
