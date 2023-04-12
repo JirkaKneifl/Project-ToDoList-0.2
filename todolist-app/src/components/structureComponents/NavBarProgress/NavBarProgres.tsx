51992b9b04e27e08b0f85b7c2275d9e0dc460ff1
Jiří Kneifl <Jirka.Kneifl@email.cz>
2023-04-12 23:49:22 +0200

    uprava gapu v navbarech

--- todolist-app/src/components/structureComponents/NavBarProgress/NavBarProgres.tsx
+++ todolist-app/src/components/structureComponents/NavBarProgress/NavBarProgres.tsx
@@ -46,7 +46,7 @@ function NavBarProgres() {
     return (
         <>
             <div className={'aboutNavBar'}>
-                <HStack alignItems={'baseline'} gap={150}>
+                <HStack alignItems={'baseline'} gap={230}>
                     <h2>
                         <Link to={`/main/${user.id}`} className={'toDoLogo'}>
                             ToDo App
